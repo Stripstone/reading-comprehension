9685b6d10c3f0b39f8fb6d7de3cf7a81d531d5d5
Triston <tristonbarker@yahoo.com>
2026-02-22 14:35:09 -0600

    cleanup last commit

--- api/anchors.js
+++
@@ -1,6 +0,0 @@
-// Vercel/Node entrypoint shim
-//
-// Some deployments may surface folder functions as /api/<name>/index.
-// This shim guarantees the canonical route /api/anchors works.
-
-export { default } from "./anchors/index.js";
